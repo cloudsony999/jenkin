74dc66b89ab8acce46ac3ed1a07f6f7f1aca4575
cloudsony999 <cloudsony999@gmail.com>
2022-06-23 11:09:34 +0530

    first commit

---
+++ jenkintry/src/main/java/a/App.java
@@ -0,0 +1,12 @@
+package a;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("java is " + System.getProperty("java.version"));
+	}
+}
